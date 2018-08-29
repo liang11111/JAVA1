74b159424f1e63b6fe16f535889cdaeefb332db6
liang11111 <liang11111@github.com>
2018-08-29 11:04:35 +0800

    测试

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public void Mirror(TreeNode root) {
+        if(root==null)
+            return;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
